abbb62f641b03474cb32bba8434fc17aa88ce6c2
Vale Carello <valentinocarello@gmail.com>
2024-07-02 17:30:34 -0300

    ajustes

--- scenes/StartScene.js
+++ scenes/StartScene.js
@@ -9,8 +9,8 @@ export default class StartScene extends Phaser.Scene {
     create() {
       // Add background or other visual elements for the start scene
   
-      this.add.text(400, 200, 'Banana Barrel', { fontSize: '80 px', fill: '#fff', fontWeight: "bold" }).setOrigin(0.5).setDepth(2);
-      this.add.text(400, 400, 'Press any key to start', { fontSize: '30px', fill: '#fff', fontWeight: "bold" }).setOrigin(0.5).setDepth(2);
+      this.add.text(400, 220, 'Banana Barrel', { fontSize: '80px', fill: '#fff', fontWeight: "bold" }).setOrigin(0.5).setDepth(2);
+      this.add.text(400, 450, 'Press any key to start', { fontSize: '30px', fill: '#fff', fontWeight: "bold" }).setOrigin(0.5).setDepth(2);
       this.add.image(400, 300, "start").setDepth(1).setScale(0.55)
       
       // Listen for keyboard events to start the game
